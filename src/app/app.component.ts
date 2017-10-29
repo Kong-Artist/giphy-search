77f99f0a0940429d626fa53f36b5e5d62cc22862
Steven Kong <FrankKwakkelaar@Franks-MacBook-Pro.local>
2017-10-30 06:32:52 +0800

    Added search input, search button and http module for Giphy requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+import { Http, Response} from '@angular/http';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Steven\'s Giphy Search';
+  query = 'http://api.giphy.com/v1/gifs/search?api_key=W9694uE7E0PQ91pv5jUgxXsn7GnJ3sSR&limit=20&q='; 
+  http: Http;
+  gifs = [];
+
+  constructor(http: Http) {
+    this.http = http;
+  }
+
+  onKey(event: any) {
+    if (event.keyCode === 13) {
+      this.searchForGifs(event.target);
+    }
+  }
+
+  // Search for Gifs using Giphy API given the input
+  searchForGifs(search: HTMLInputElement) : void {
+    var apiLink = this.query + search.value; 
+    this.http.request(apiLink) 
+      .subscribe((res: Response) => { 
+        this.gifs = res.json().data;
+      }); 
+  }
 }
